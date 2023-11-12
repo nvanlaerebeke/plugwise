82b7be6b83af8bbf7eef502dc0c64d7e2c2a42c7
Nico Van Laerebeke <nico.vanlaerebeke@nomadesk.com>
2023-11-12 20:48:28 +0100

    added name to the circleinfo object;

--- src/Plugwise.Api/Objects/PlugInfo.cs
+++ src/Plugwise.Api/Objects/PlugInfo.cs
@@ -7,10 +7,14 @@ namespace Plugwise.Api.Objects;
 
 public class PlugInfo : PlugMetric {
     public PlugInfo(Plug plug, Usage usage, CircleInfoResponse circleInfo) : base(plug, usage) {
+        Mac = plug.Mac;
+        Name = plug.Name;
         AllowUpdateState = plug.PowerControl;
         SwitchState = circleInfo.State;
-        Mac = plug.Mac;
     }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; }
     
     [JsonPropertyName("mac")]
     public string Mac { get; }
